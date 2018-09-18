62c6f83213e2f5e22121cb2bac45955026a1134e
Akbinar, Ali <ali.akbinar@rabobank.nl>
2018-09-18 18:47:12 +0200

    eerste commit

---
+++ src/main/java/nl/hhs/project/koffieapp/koffieapp/KoffieappApplication.java
@@ -0,0 +1,12 @@
+package nl.hhs.project.koffieapp.koffieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoffieappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoffieappApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/hhs/project/koffieapp/koffieapp/KoffieappApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.hhs.project.koffieapp.koffieapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KoffieappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
