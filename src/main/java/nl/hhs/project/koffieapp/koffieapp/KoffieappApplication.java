225672d1544a440cfa334050fa1c74380e900aca
Akbinar, Ali <aliakbinar@hotmail.com>
2018-09-18 19:01:18 +0200

    remove hallo

--- src/main/java/nl/hhs/project/koffieapp/koffieapp/KoffieappApplication.java
+++ src/main/java/nl/hhs/project/koffieapp/koffieapp/KoffieappApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KoffieappApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hallo");
 		SpringApplication.run(KoffieappApplication.class, args);
 	}
 }
