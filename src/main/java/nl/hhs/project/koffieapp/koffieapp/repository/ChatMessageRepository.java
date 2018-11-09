1a1d262fbeffc17ba0248e6f99a4547c6590ffda
TimBlanken <tim.blanken@alvant.nl>
2018-11-09 09:22:34 +0100

    add departmentController and Canvas model + controller

--- src/main/java/nl/hhs/project/koffieapp/koffieapp/repository/ChatMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package nl.hhs.project.koffieapp.koffieapp.repository;
-
-import nl.hhs.project.koffieapp.koffieapp.model.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-}
