9aad6093ebd5be5cd7d70259f00decb0b596005f
HuubVerdel <huubverdel@gmail.com>
2019-01-22 12:48:35 +0100

    Update AuthController.java

--- src/main/java/nl/hhs/project/koffieapp/koffieapp/controller/rest/AuthController.java
+++ src/main/java/nl/hhs/project/koffieapp/koffieapp/controller/rest/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/auth")
 @CrossOrigin(origins = "http://ec2-18-191-130-224.us-east-2.compute.amazonaws.com:4200")
 //@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "${co.url}")
 class AuthController {
 
 
